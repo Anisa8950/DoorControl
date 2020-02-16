82852d9ff35cb877d5e52fd5ff8585f4ff4a8a53
Stonani <stefine.varde@gmail.com>
2020-02-16 14:41:01 +0100

    Add project files.

---
+++ DoorControlFakes2/Interfaces/IEntryNotification.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorControlFakes2
+{
+    public interface IEntryNotification
+    {
+    }
+}
